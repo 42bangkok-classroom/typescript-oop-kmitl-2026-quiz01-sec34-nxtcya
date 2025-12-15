4ad08ff557b643ba3437783155239c2366f6af72
nxtcya <quinn3nxt@gmail.com>
2025-12-15 17:51:29 +0700

    hasAt.ts

---
+++ src/q11/hasAt.ts
@@ -0,0 +1,10 @@
+const input = process.argv[2];
+if(!input){
+  console.log("Please provide a string")
+  process.exit;
+}
+if(input.includes("@")){
+  console.log("Has@");
+}else{
+  console.log("No@");
+}
